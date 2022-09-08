67e5cbc2cf2a364881e22aabdb547211eeba3fd8
tukta <nattakamon.j@rmutsvmail.com>
2022-09-08 16:39:08 +0700

    Lad4: List

---
+++ Lab4/ex_list3.py
@@ -0,0 +1,18 @@
+"""
+Name : Anutta Thongdet
+ID : 364411760003
+Group : MIT421
+"""
+
+# list comprehension
+
+num = []
+
+for x in range(1,101): # 1-100
+    num.append(x)
+
+print(num)
+
+
+newlist = [x for x in num if x%3==0]
+print(newlist)
